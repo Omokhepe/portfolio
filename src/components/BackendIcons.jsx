2c265d018048774a2e66370f0764e589ed226168
omoh_khepe <fimobu@gmail.com>
2025-02-20 13:49:41 +0800

    first commit

---
+++ src/components/BackendIcons.jsx
@@ -0,0 +1,50 @@
+const BackendIcons = () => {
+    return (
+        <div className="flex justify-center md:justify-start my-10 gap-7">
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                {/*<div className="bg-[url(/assets/api.png)]"></div>*/}
+                <img alt="linkedin-link" src="../assets/api.png" />
+            </a>
+            {/*<a*/}
+            {/*    className="hover:opacity-50 transition duration-500"*/}
+            {/*    href="https://www.twitter.com"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noreferrer"*/}
+            {/*>*/}
+            {/*    <img alt="twitter-link" src="../assets/asp.png" />*/}
+            {/*</a>*/}
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="facebook-link" className='inline-block rounded-lg bg-grey p-2' src="../assets/expressjs.png" />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="instagram-link" className='inline-block rounded-lg bg-grey p-2' src="../assets/postgre.png" />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="instagram-link" className='inline-block rounded-lg bg-grey p-2' src="../assets/sql.png" />
+            </a>
+
+        </div>
+    );
+};
+
+export default BackendIcons;
---
+++ src/components/FrontendIcons.jsx
@@ -0,0 +1,56 @@
+const FrontendIcons = () => {
+    return (
+        <div className="flex justify-center md:justify-start my-10 gap-7">
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="linkedin-link" src="../assets/html5.png" />
+            </a>
+            <a
+              className="hover:opacity-50 transition duration-500"
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="twitter-link" src="../assets/css3.png" />
+            </a>
+            <a
+              className="hover:opacity-50 transition duration-500"
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="facebook-link" src="../assets/typescript.png" />
+            </a>
+            <a
+              className="hover:opacity-50 transition duration-500"
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="instagram-link" src="../assets/javascript.png" />
+            </a>
+            <a
+              className="hover:opacity-50 transition duration-500"
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="instagram-link" src="../assets/tailwindcss.png" />
+            </a>
+            <a
+              className="hover:opacity-50 transition duration-500"
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img alt="instagram-link" src="../assets/redux.png" />
+            </a>
+        </div>
+    );
+};
+
+export default FrontendIcons;
